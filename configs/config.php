0ffdc7f8199434675d4e63d373ff3df6d0fd02e1
Basil <git@nadar.io>
2020-02-27 11:37:55 +0100

    Update config.php

--- configs/config.php
+++ configs/config.php
@@ -19,6 +19,7 @@ $config = new Config('myproject', dirname(__DIR__), [
             'secureLogin' => false, // when enabling secure login, the mail component must be proper configured otherwise the auth token mail will not send.
             'strongPasswordPolicy' => false, // If enabled, the admin user passwords require strength input with special chars, lower, upper, digits and numbers
             'interfaceLanguage' => 'en', // Admin interface default language. Currently supported: en, de, ru, es, fr, ua, it, el, vi, pt, fa
+            'autoBootstrapQueue' => true, // Enables the fake cronjob by default, read more about queue/scheduler: https://luya.io/guide/app-queue
         ],
         // Frontend module for the `cms` module.
         'cms' => [
